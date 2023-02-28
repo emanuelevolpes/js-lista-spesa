238c7097f20739f123b4a3a59776756c8c47f36d
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-02-28 16:09:07 +0100

    bonus list for/while/do-while

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const shoppingList = [
+//LIST-WHILE
+const shoppingListWhile = [
     'Pane',
     'Pasta',
     'Pollo',
@@ -9,13 +10,52 @@ const shoppingList = [
     'Pesce'
 ];
 
-let containerList = document.getElementById('shopping-list');
+let containerList = document.getElementById('shopping-list-while');
 let counter = 0;
 
-while (counter < shoppingList.length) {
-    console.log(shoppingList[counter]);
+while (counter < shoppingListWhile.length) {
+    console.log(shoppingListWhile[counter]);
     const listItem = document.createElement('li');
-    listItem.append(shoppingList[counter]);
+    listItem.append(shoppingListWhile[counter]);
     containerList.append(listItem);
     counter++;
 }
+
+
+//LIST-FOR
+const shoppingListFor = [
+    'Pane',
+    'Pasta',
+    'Pollo',
+    'Latte',
+    'Riso',
+    'Pesce'
+];
+
+let containerListFor = document.getElementById('shopping-list-for');
+
+for (let i = 0; i < shoppingListFor.length; i++) {
+    const listItemFor = document.createElement('li');
+    listItemFor.append(shoppingListFor[i]);
+    containerListFor.append(listItemFor);
+}
+
+//LIST-DO-WHILE
+const shoppingListDoWhile = [
+    'Pane',
+    'Pasta',
+    'Pollo',
+    'Latte',
+    'Riso',
+    'Pesce'
+];
+
+let containerListDoWhile = document.getElementById('shopping-list-do-while');
+let doWhileCounter = 0;
+
+do {
+    const listItemDoWhile = document.createElement('li');
+    listItemDoWhile.append(shoppingListDoWhile[doWhileCounter]);
+    containerListDoWhile.append(listItemDoWhile);
+    doWhileCounter++;
+} while (doWhileCounter < shoppingListDoWhile.length);
